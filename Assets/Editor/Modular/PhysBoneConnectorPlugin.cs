6d57a9ee52b9b1097314d8f2b0f5bbfee5909b69
Macchiato <simppp@gmail.com>
2026-03-31 10:07:49 +0900

    PhysBone을 연결해 주는 컴포넌트 생성

---
+++ Assets/Editor/Modular/PhysBoneConnectorPlugin.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Linq;
+
+using UnityEditor;
+using UnityEngine;
+
+using VRC.SDK3.Avatars.Components;
+
+using nadena.dev.ndmf;
+using nadena.dev.ndmf.vrchat;
+
+using Object = UnityEngine.Object;
+
+/*
+ * VRSuya Modular Component
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+[assembly: ExportsPlugin(typeof(VRSuya.Modular.Editor.PhysBoneConnectorPlugin))]
+
+namespace VRSuya.Modular.Editor {
+
+    public class PhysBoneConnectorPlugin : Plugin<PhysBoneConnectorPlugin> {
+
+		public override string QualifiedName => "com.vrsuya.modular.physboneconnector";
+		public override string DisplayName => "VRSuya PhysBone Connector";
+
+		protected override void Configure() {
+			InPhase(BuildPhase.Resolving).Run(PhysBoneConnectorPass.Instance);
+		}
+	}
+
+	public class PhysBoneConnectorPass : Pass<PhysBoneConnectorPass> {
+
+		public override string DisplayName => "PhysBone Connector";
+
+		static readonly string[] Cheek_L_Names = new string[] { "Cheek.L", "Cheek1_L", "Cheek_Root_L", "Cheek_root_L" };
+		static readonly string[] Cheek_R_Names = new string[] { "Cheek.R", "Cheek1_R", "Cheek_Root_R", "Cheek_root_R" };
+
+		protected override void Execute(BuildContext TargetBuildContext) {
+			TargetBuildContext.AvatarRootObject.TryGetComponent(out Animator TargetAnimator);
+			if (!TargetAnimator) return;
+			PhysBoneConnector[] PhysBoneConnectors = TargetBuildContext.AvatarRootObject.GetComponentsInChildren<PhysBoneConnector>();
+			foreach (PhysBoneConnector TargetComponent in PhysBoneConnectors) {
+				if (!TargetComponent) continue;
+				if (TargetComponent.TargetType == PhysBoneType.Cheek) {
+					Transform[] HeadTransforms = TargetAnimator.GetBoneTransform(HumanBodyBones.Head).GetComponentsInChildren<Transform>(true);
+					Transform Cheek_L_Transform = HeadTransforms.FirstOrDefault(Item => Cheek_L_Names.Contains(Item.name));
+					Transform Cheek_R_Transform = HeadTransforms.FirstOrDefault(Item => Cheek_R_Names.Contains(Item.name));
+					if (Cheek_L_Transform && TargetComponent.TargetCheek_L) {
+						TargetComponent.TargetCheek_L.rootTransform = Cheek_L_Transform;
+					}
+					if (Cheek_R_Transform && TargetComponent.TargetCheek_R) {
+						TargetComponent.TargetCheek_R.rootTransform = Cheek_R_Transform;
+					}
+				} else {
+					Transform[] Toe_L_Transform = TargetAnimator.GetBoneTransform(HumanBodyBones.LeftToes).GetComponentsInChildren<Transform>(true);
+					Transform[] Toe_R_Transform = TargetAnimator.GetBoneTransform(HumanBodyBones.RightToes).GetComponentsInChildren<Transform>(true);
+					Transform ThumbToe1_L_Transform = Toe_L_Transform.FirstOrDefault(Item => Item.name == "ThumbToe1_L" || Item.name == "Toe_Thumb_Proximal_L");
+					Transform ThumbToe1_R_Transform = Toe_R_Transform.FirstOrDefault(Item => Item.name == "ThumbToe1_R" || Item.name == "Toe_Thumb_Proximal_R");
+					Transform IndexToe1_L_Transform = Toe_L_Transform.FirstOrDefault(Item => Item.name == "IndexToe1_L" || Item.name == "Toe_Index_Proximal_L");
+					Transform IndexToe1_R_Transform = Toe_R_Transform.FirstOrDefault(Item => Item.name == "IndexToe1_R" || Item.name == "Toe_Index_Proximal_R");
+					Transform MiddleToe1_L_Transform = Toe_L_Transform.FirstOrDefault(Item => Item.name == "MiddleToe1_L" || Item.name == "Toe_Middle_Proximal_L");
+					Transform MiddleToe1_R_Transform = Toe_R_Transform.FirstOrDefault(Item => Item.name == "MiddleToe1_R" || Item.name == "Toe_Middle_Proximal_R");
+					Transform RingToe1_L_Transform = Toe_L_Transform.FirstOrDefault(Item => Item.name == "RingToe1_L" || Item.name == "Toe_Ring_Proximal_L");
+					Transform RingToe1_R_Transform = Toe_R_Transform.FirstOrDefault(Item => Item.name == "RingToe1_R" || Item.name == "Toe_Ring_Proximal_R");
+					Transform LittleToe1_L_Transform = Toe_L_Transform.FirstOrDefault(Item => Item.name == "LittleToe1_L" || Item.name == "Toe_Little_Proximal_L");
+					Transform LittleToe1_R_Transform = Toe_R_Transform.FirstOrDefault(Item => Item.name == "LittleToe1_R" || Item.name == "Toe_Little_Proximal_R");
+					if (ThumbToe1_L_Transform && TargetComponent.TargetThumbToe1_L) {
+						TargetComponent.TargetThumbToe1_L.rootTransform = ThumbToe1_L_Transform;
+					}
+					if (ThumbToe1_R_Transform && TargetComponent.TargetThumbToe1_R) {
+						TargetComponent.TargetThumbToe1_R.rootTransform = ThumbToe1_R_Transform;
+					}
+					if (IndexToe1_L_Transform && TargetComponent.TargetIndexToe1_L) {
+						TargetComponent.TargetIndexToe1_L.rootTransform = IndexToe1_L_Transform;
+					}
+					if (IndexToe1_R_Transform && TargetComponent.TargetIndexToe1_R) {
+						TargetComponent.TargetIndexToe1_R.rootTransform = IndexToe1_R_Transform;
+					}
+					if (MiddleToe1_L_Transform && TargetComponent.TargetMiddleToe1_L) {
+						TargetComponent.TargetMiddleToe1_L.rootTransform = MiddleToe1_L_Transform;
+					}
+					if (MiddleToe1_R_Transform && TargetComponent.TargetMiddleToe1_R) {
+						TargetComponent.TargetMiddleToe1_R.rootTransform = MiddleToe1_R_Transform;
+					}
+					if (RingToe1_L_Transform && TargetComponent.TargetRingToe1_L) {
+						TargetComponent.TargetRingToe1_L.rootTransform = RingToe1_L_Transform;
+					}
+					if (RingToe1_R_Transform && TargetComponent.TargetRingToe1_R) {
+						TargetComponent.TargetRingToe1_R.rootTransform = RingToe1_R_Transform;
+					}
+					if (LittleToe1_L_Transform && TargetComponent.TargetLittleToe1_L) {
+						TargetComponent.TargetLittleToe1_L.rootTransform = LittleToe1_L_Transform;
+					}
+					if (LittleToe1_R_Transform && TargetComponent.TargetLittleToe1_R) {
+						TargetComponent.TargetLittleToe1_R.rootTransform = LittleToe1_R_Transform;
+					}
+				}
+				Object.DestroyImmediate(TargetComponent);
+			}
+		}
+	}
+
+	[CustomEditor(typeof(PhysBoneConnector))]
+	public class PhysBoneConnectorEditor : UnityEditor.Editor {
+
+		SerializedProperty SerializedTargetType;
+
+		SerializedProperty SerializedTargetCheek_L;
+		SerializedProperty SerializedTargetCheek_R;
+
+		SerializedProperty SerializedTargetThumbToe1_L;
+		SerializedProperty SerializedTargetThumbToe1_R;
+		SerializedProperty SerializedTargetIndexToe1_L;
+		SerializedProperty SerializedTargetIndexToe1_R;
+		SerializedProperty SerializedTargetMiddleToe1_L;
+		SerializedProperty SerializedTargetMiddleToe1_R;
+		SerializedProperty SerializedTargetRingToe1_L;
+		SerializedProperty SerializedTargetRingToe1_R;
+		SerializedProperty SerializedTargetLittleToe1_L;
+		SerializedProperty SerializedTargetLittleToe1_R;
+
+		void OnEnable() {
+			SerializedTargetType = serializedObject.FindProperty("TargetType");
+
+			SerializedTargetCheek_L = serializedObject.FindProperty("TargetCheek_L");
+			SerializedTargetCheek_R = serializedObject.FindProperty("TargetCheek_R");
+
+			SerializedTargetThumbToe1_L = serializedObject.FindProperty("TargetThumbToe1_L");
+			SerializedTargetThumbToe1_R = serializedObject.FindProperty("TargetThumbToe1_R");
+			SerializedTargetIndexToe1_L = serializedObject.FindProperty("TargetIndexToe1_L");
+			SerializedTargetIndexToe1_R = serializedObject.FindProperty("TargetIndexToe1_R");
+			SerializedTargetMiddleToe1_L = serializedObject.FindProperty("TargetMiddleToe1_L");
+			SerializedTargetMiddleToe1_R = serializedObject.FindProperty("TargetMiddleToe1_R");
+			SerializedTargetRingToe1_L = serializedObject.FindProperty("TargetRingToe1_L");
+			SerializedTargetRingToe1_R = serializedObject.FindProperty("TargetRingToe1_R");
+			SerializedTargetLittleToe1_L = serializedObject.FindProperty("TargetLittleToe1_L");
+			SerializedTargetLittleToe1_R = serializedObject.FindProperty("TargetLittleToe1_R");
+		}
+
+		public override void OnInspectorGUI() {
+			serializedObject.Update();
+			EditorGUILayout.PropertyField(SerializedTargetType, new GUIContent("PhysBone Type"));
+			EditorGUILayout.Space();
+			PhysBoneType TargetType = (PhysBoneType)SerializedTargetType.enumValueIndex;
+			using (new EditorGUILayout.VerticalScope(EditorStyles.helpBox)) {
+				if (TargetType == 0) {
+					EditorGUILayout.PropertyField(SerializedTargetCheek_L, new GUIContent("Left Cheek"));
+					EditorGUILayout.PropertyField(SerializedTargetCheek_R, new GUIContent("Right Cheek"));
+				} else {
+					EditorGUILayout.PropertyField(SerializedTargetThumbToe1_L, new GUIContent("Left ThumbToe"));
+					EditorGUILayout.PropertyField(SerializedTargetThumbToe1_R, new GUIContent("Right ThumbToe"));
+					EditorGUILayout.PropertyField(SerializedTargetIndexToe1_L, new GUIContent("Left IndexToe"));
+					EditorGUILayout.PropertyField(SerializedTargetIndexToe1_R, new GUIContent("Right IndexToe"));
+					EditorGUILayout.PropertyField(SerializedTargetMiddleToe1_L, new GUIContent("Left MiddleToe"));
+					EditorGUILayout.PropertyField(SerializedTargetMiddleToe1_R, new GUIContent("Right MiddleToe"));
+					EditorGUILayout.PropertyField(SerializedTargetRingToe1_L, new GUIContent("Left RingToe"));
+					EditorGUILayout.PropertyField(SerializedTargetRingToe1_R, new GUIContent("Right RingToe"));
+					EditorGUILayout.PropertyField(SerializedTargetLittleToe1_L, new GUIContent("Left LittleToe"));
+					EditorGUILayout.PropertyField(SerializedTargetLittleToe1_R, new GUIContent("Right LittleToe"));
+				}
+			}
+			serializedObject.ApplyModifiedProperties();
+		}
+	}
+}
---
+++ Assets/Runtime/Modular/PhysBoneConnector.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+using VRC.SDK3.Dynamics.PhysBone.Components;
+
+/*
+ * VRSuya Modular Component
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+namespace VRSuya.Modular {
+
+	public enum PhysBoneType {
+		Cheek, Toe
+	}
+
+	[AddComponentMenu("VRSuya/Modular/VRSuya PhysBone Connector")]
+	[HelpURL("https://vrsuya.booth.pm/")]
+	public class PhysBoneConnector : MonoBehaviour {
+		public PhysBoneType TargetType = PhysBoneType.Cheek;
+		public VRCPhysBone TargetCheek_L;
+		public VRCPhysBone TargetCheek_R;
+		public VRCPhysBone TargetThumbToe1_L;
+		public VRCPhysBone TargetThumbToe1_R;
+		public VRCPhysBone TargetIndexToe1_L;
+		public VRCPhysBone TargetIndexToe1_R;
+		public VRCPhysBone TargetMiddleToe1_L;
+		public VRCPhysBone TargetMiddleToe1_R;
+		public VRCPhysBone TargetRingToe1_L;
+		public VRCPhysBone TargetRingToe1_R;
+		public VRCPhysBone TargetLittleToe1_L;
+		public VRCPhysBone TargetLittleToe1_R;
+	}
+}
