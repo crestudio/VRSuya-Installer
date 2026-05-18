10e5c53b423dab940774f97e5317fafb73f8210a
Macchiato <simppp@gmail.com>
2026-05-19 01:30:43 +0900

    FX 레이어에서 마스크를 제거하는 컴포넌트 추가

---
+++ Assets/Editor/Modular/RemoveFXMaskPlugin.cs
@@ -0,0 +1,92 @@
+﻿#if MODULAR_AVATAR
+using System.Linq;
+
+using UnityEditor;
+using UnityEditor.Animations;
+
+using VRC.SDK3.Avatars.Components;
+
+using nadena.dev.ndmf;
+
+using static VRSuya.Core.Translator;
+
+using Avatar = VRSuya.Core.Avatar;
+using Object = UnityEngine.Object;
+
+/*
+ * VRSuya Modular Component
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+[assembly: ExportsPlugin(typeof(VRSuya.Modular.Editor.RemoveFXMaskPlugin))]
+
+namespace VRSuya.Modular.Editor {
+
+    public class RemoveFXMaskPlugin : Plugin<RemoveFXMaskPlugin> {
+
+		public override string QualifiedName => "com.vrsuya.modular.removefxmask";
+		public override string DisplayName => "VRSuya RemoveFXMask";
+
+		protected override void Configure() {
+			InPhase(BuildPhase.Generating).Run(RemoveFXMaskPass.Instance);
+		}
+	}
+
+	public class RemoveFXMaskPass : Pass<RemoveFXMaskPass> {
+
+		public override string DisplayName => "RemoveFXMask";
+
+		protected override void Execute(BuildContext TargetBuildContext) {
+			RemoveFXMask[] RemoveFXMaskComponents = TargetBuildContext.AvatarRootObject.GetComponentsInChildren<RemoveFXMask>(true);
+			if (RemoveFXMaskComponents.Length > 0) {
+				Avatar AvatarInstance = new Avatar();
+				AnimatorController TargetAnimator = AvatarInstance.GetAnimatorController(TargetBuildContext.AvatarRootObject, VRCAvatarDescriptor.AnimLayerType.FX);
+				if (TargetAnimator) {
+					if (RemoveFXMask(TargetAnimator)) {
+						AssetDatabase.SaveAssets();
+					}
+				}
+				foreach (RemoveFXMask TargetComponent in RemoveFXMaskComponents) {
+					if (TargetComponent) Object.DestroyImmediate(TargetComponent);
+				}
+			}
+		}
+
+		bool RemoveFXMask(AnimatorController TargetAnimator) {
+			if (TargetAnimator.layers.Length > 0) {
+				int FXMaskCount = TargetAnimator.layers.Count(Item => Item.avatarMask != null);
+				if (FXMaskCount > 0) {
+					AnimatorControllerLayer[] NewAnimatorLayers = new AnimatorControllerLayer[TargetAnimator.layers.Length];
+					for (int Index = 0; Index < TargetAnimator.layers.Length; Index++) {
+						AnimatorControllerLayer NewLayer = new AnimatorControllerLayer {
+							avatarMask = null,
+							blendingMode = TargetAnimator.layers[Index].blendingMode,
+							defaultWeight = TargetAnimator.layers[Index].defaultWeight,
+							iKPass = TargetAnimator.layers[Index].iKPass,
+							name = TargetAnimator.layers[Index].name,
+							stateMachine = TargetAnimator.layers[Index].stateMachine,
+							syncedLayerAffectsTiming = TargetAnimator.layers[Index].syncedLayerAffectsTiming,
+							syncedLayerIndex = TargetAnimator.layers[Index].syncedLayerIndex
+						};
+						NewAnimatorLayers[Index] = NewLayer;
+					}
+					TargetAnimator.layers = NewAnimatorLayers;
+					EditorUtility.SetDirty(TargetAnimator);
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+	[CustomEditor(typeof(RemoveFXMask))]
+	public class RemoveFXMaskEditor : UnityEditor.Editor {
+
+		public override void OnInspectorGUI() {
+			LanguageIndex = EditorGUILayout.Popup(GetTranslatedString("String_Language"), LanguageIndex, LanguageOption);
+			EditorGUILayout.Space();
+			EditorGUILayout.HelpBox(GetTranslatedString("String_RemoveFXMask"), MessageType.Info);
+		}
+	}
+}
+#endif
---
+++ Assets/Runtime/Modular/RemoveFXMask.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+using VRC.SDKBase;
+
+/*
+ * VRSuya Modular Component
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+namespace VRSuya.Modular {
+
+	[AddComponentMenu("VRSuya/Modular/VRSuya RemoveFXMask")]
+	[HelpURL("https://vrsuya.booth.pm/")]
+	public class RemoveFXMask : MonoBehaviour, IEditorOnly {
+
+	}
+}
