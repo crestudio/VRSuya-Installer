142db166311b136d470cc11af739f9f23bcedccc
Macchiato <simppp@gmail.com>
2026-06-01 20:54:27 +0900

    Yoll 볼 이름 추가

--- Assets/Editor/Modular/PhysBoneConnectorPlugin.cs
+++ Assets/Editor/Modular/PhysBoneConnectorPlugin.cs
@@ -42,8 +42,8 @@ namespace VRSuya.Modular.Editor {
 					foreach (PhysBoneConnector TargetComponent in PhysBoneConnectors) {
 						if (!TargetComponent) continue;
 						if (TargetComponent.TargetType == PhysBoneType.Cheek) {
-							string[] Cheek_L_Names = new string[] { "Cheek.L", "Cheek1_L", "Cheek_Root_L", "Cheek_root_L", "Hoppe.L" };
-							string[] Cheek_R_Names = new string[] { "Cheek.R", "Cheek1_R", "Cheek_Root_R", "Cheek_root_R", "Hoppe.R" };
+							string[] Cheek_L_Names = new string[] { "Cheek.L", "Cheek1_L", "Cheek_Root_L", "Cheek_root_L", "Hoppe.L", "ho_L" };
+							string[] Cheek_R_Names = new string[] { "Cheek.R", "Cheek1_R", "Cheek_Root_R", "Cheek_root_R", "Hoppe.R", "ho_R" };
 							Transform[] HeadTransforms = TargetAnimator.GetBoneTransform(HumanBodyBones.Head).GetComponentsInChildren<Transform>(true);
 							Transform Cheek_L_Transform = HeadTransforms.FirstOrDefault(Item => Cheek_L_Names.Contains(Item.name));
 							Transform Cheek_R_Transform = HeadTransforms.FirstOrDefault(Item => Cheek_R_Names.Contains(Item.name));
